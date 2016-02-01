37cc1198bf4b18d8464bd313716b7b25058c0184
Ben Short <ben.is.not.short@gmail.com>
2016-02-01 15:45:17 -0500

    Fixed the style
    FIxed the style

--- src/Subsystems/ExampleSubsystem.cpp
+++ src/Subsystems/ExampleSubsystem.cpp
@@ -1,14 +1,11 @@
 #include "ExampleSubsystem.h"
 #include "../RobotMap.h"
 
-ExampleSubsystem::ExampleSubsystem() :
-		Subsystem("ExampleSubsystem")
-{
+ExampleSubsystem::ExampleSubsystem() :Subsystem("ExampleSubsystem") {
 
 }
 
-void ExampleSubsystem::InitDefaultCommand()
-{
+void ExampleSubsystem::InitDefaultCommand() {
 	// Set the default command for a subsystem here.
 	//SetDefaultCommand(new MySpecialCommand());
 }
