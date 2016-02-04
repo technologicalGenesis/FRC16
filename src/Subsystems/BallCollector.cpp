e60d23fc249964b81d68c2a86925a1e62068c16e
Chris Beeler <chrisbeeler627@comcast.net>
2016-02-04 16:47:31 -0500

    Finished again(?)
    Followed more comments

---
+++ src/Subsystems/BallCollector.cpp
@@ -0,0 +1,14 @@
+#include "BallCollector.h"
+#include "../RobotMap.h"
+
+BallCollector::BallCollector(): Subsystem("BallCollector") {
+	motor = new Talon(RobotMap::INTAKE_MOTOR);
+
+}
+
+void BallCollector::Start() {
+	motor->Set(1);
+}
+void BallCollector::Stop() {
+	motor->Set(0);
+}
---
+++ src/Subsystems/BallCollector.h
@@ -0,0 +1,19 @@
+#ifndef BallCollector_H
+#define BallCollector_H
+
+#include "Commands/Subsystem.h"
+#include "WPILib.h"
+
+class BallCollector: Subsystem {
+
+public:
+	Talon* motor;
+
+	BallCollector();
+
+	void Start();
+	void Stop();
+
+};
+
+#endif
