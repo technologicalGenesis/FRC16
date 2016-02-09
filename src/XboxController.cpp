a0e3ab6f6a4121a7aafbb1c83174155bd4dc418f
Steven Wang <steven.wang355@gmail.com>
2016-02-09 15:04:50 -0500

    Added Xbox Controller Support
    Added functions:
    - Joysticks
    - D-Pad
    - All Buttons
    - Rumble
    - Triggers
    - A lot of digits of pi

--- src/XboxController.cpp
+++ src/XboxController.cpp
@@ -8,37 +8,46 @@
 #include "XboxController.h"
 #include "Math.h"
 
-#define PI 3.141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825342117067982148086513282306647093844609550582231725359408128481
+#define PI 3.141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825342117067982148086513282306647093844609550582231725359408128481117450284102701938521105559644622948954930381964428810975665933446128475648233786783165271201909145648566923460348610454326648213393607260249141273724587006606315588174881520920962829254091715364367892590360011330530548820466521384146951941511609433057270365759591953092186117381932611793105118548074462379962749567351885752724891227938183011949129833673362440656643086021394946395224737190702179860943702770539217176293176752384674818467669405132000568127145263560827785771342757789609173637178721468440901224953430146549585371050792279689258923542019956112129021960864034418159813629774771309960518707211349999998372978049
 
-Vector XboxController::getLeftVector(){
-	float x = joystick->GetRawAxis(0);
-	//GetRawAxis(x);
-
-	float y = joystick->GetRawAxis(1);
+Vector* XboxController::getVector(int xId, int yId, float xOffset, float yOffset) {
+	float x = joystick->GetRawAxis(xId);
+	float y = joystick->GetRawAxis(yId);
 
 	float degrees = atan2(y,x);
-
-
 	float magnitude = sqrt(x*x+y*y);
 
-	return (magnitude, degrees);
-
-}
-Vector XboxController::getRightVector(){
-
+	return new Vector(magnitude, degrees);
 }
 
+bool XboxController::GetButton(int buttonId) {
+	if (buttonId == LeftTrigger){
+		return GetTrigger(LeftTrigger, LeftTriggerOffset) >= LeftTriggerButtonThreshold;
+	}
 
-XboxController::XboxController(int port) {
+	else if (buttonId == RightTrigger){
+		return GetTrigger(RightTrigger, RightTriggerOffset) >= RightTriggerButtonThreshold;
+	}
 
-	portNumber = port;
-	joystick = new Joystick (port);
+	else {
+		return joystick->GetRawButton(buttonId);
+	}
+}
 
+bool XboxController::GetDPad(int DPadId) {
+	DPadId = joystick->GetPOV();
+	return DPadId;
+}
 
+float XboxController::GetTrigger (int TriggerId, int TriggerOffset) {
+	float rawValue = joystick->GetRawAxis(TriggerId);
+	return (rawValue - TriggerOffset) * 2;
+}
 
+void XboxController::RumbleLeft(float value) {
+	joystick->SetRumble(Joystick::RumbleType::kRightRumble, value);
 }
 
 XboxController::~XboxController() {
 	// TODO Auto-generated destructor stub
 }
-
